c6187d795b19f072ff0fb0795d79ca8f965ef0f5
unknown <vijayks2020@gmail.com>
2019-04-25 16:45:47 +0530

    initial commit

--- application.py
+++ application.py
@@ -1,202 +1,209 @@
-from flask import Flask,request, render_template, redirect, url_for,make_response,send_from_directory,send_file
-from flask import Response
-from flask import jsonify
-import urllib.request
-import requests
+from flask import Flask,request,jsonify
 import sys
 import time
+# import MsSqlConnect
+import json
+app = Flask(__name__)
+
 from zeep import Client
 import zeep
 from zeep.wsse.username import UsernameToken
 
-import json
-from fullcontact import FullContact
-
-
-import urllib.request, json
-
+client = Client('https://ws.staging.training.gov.au/Deewr.Tga.WebServices/OrganisationServiceV7.svc?wsdl', wsse=UsernameToken('WebService.Read', 'Asdf098'))
 
 import pyodbc
+
 server = 'dbserveranasight.database.windows.net'
 database = 'warehouse'
 username = 'anasight@dbserveranasight'
 password = 'Root@12345'
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
+# print ('Reading data from table')
+# tsql = "SELECT * FROM jobready_parties;"
+# with cursor.execute(tsql):
+#     row = cursor.fetchone()
+#     print(row)
+# sys.exit()
+#Get server Time
+@app.route("/getServerTime",methods = ['POST', 'GET'])
+def getServerTime():
+    if request.method == 'GET':
+        return jsonify({'type': 'Get Server Time', 'responseData': client.service.GetServerTime(), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
 
-fc = FullContact('0DHVXQOcAgZzZbUPTeTQ83AakHlzIE8L')
-APIKey='0DHVXQOcAgZzZbUPTeTQ83AakHlzIE8L'
-app = Flask(__name__)
-
-#************ Get Contact details By Email *******
-@app.route("/person_enrich", methods=['POST','GET'])
-def person_enrich():
-    if request.method == 'POST':
+#search Result
+@app.route("/searchResult", methods=['POST', 'GET'])
+def searchResult():
+    if request.method == 'GET':
         jsonData = request.get_json(force=True)
-        email1 =jsonData['email']
-        if email1 == "" :
-            return jsonify({'type': 'validation','message': 'Email is required','status': 0})
-            sys.exit()
+        getServerTime=client.service.Search()
 
-        if  email1 !="":
-            headers = {
-                'Authorization': 'Bearer '+APIKey,
-            }
-            params = {
-                'email': email1,
-            }
-            payload = {
-
-                'email': email1,
-            }
-            url = 'https://api.fullcontact.com/v3/person.enrich'
-            response = requests.post(url, headers=headers, params=params,
-                                     data=json.dumps(payload))
-            response.raise_for_status()
-            res=response.json()
-
-            generate_insert_query('fullcontact_person_enrich', res, cursor)
-            personalData={
-                'fullName':res['fullName'],
-                'ageRange': res['ageRange'],
-                'gender': res['gender'],
-                'location': res['location'],
-                'title': res['title'],
-                'organization': res['organization'],
-                'twitter': res['twitter'],
-                'facebook': res['facebook'],
-                'avatar': res['avatar'],
-                'website': res['website'],
-                'updated': res['updated'],
-                'bio': res['bio'],
-                'linkedin': res['linkedin'],
-
-            }
-            dataAddOnsArr=res['dataAddOns']
-            detailsArr = res['details']
-            return jsonify({'detailsArr':detailsArr,'dataAddOnsArr':dataAddOnsArr,'personalData':personalData})
-            sys.exit()
+        return jsonify({'type': 'search Result', 'responseData': getServerTime, 'status': 1})
+    else:
+        return jsonify({'type': 'request', 'message': 'Sorry! Method Not Allowed', 'status': '0'})
 
 
+#Get Address States
+@app.route("/GetAddressStates",methods = ['POST', 'GET'])
+def GetAddressStates():
+    if request.method == 'GET':
+        addressStates=[]
+        addressStates=client.service.GetAddressStates()
+        #Generate QUERY for each dictionary inside data list
 
-        else:
-            return jsonify({'message':'validation Error','status':0})
-            sys.exit()
-    return jsonify({'error': 'Method are not allowed', 'status': 0})
-    sys.exit()
-#************ Get Contact details By Email End*******
+        for query in addressStates:
+            generate_insert_query('tga_address_states',query,cursor)
 
+        return jsonify({'type': 'Get Address States', 'responseData': str(addressStates), 'status': 1})
 
-#************ Get Contact details By Phone *******
-@app.route("/phone_details", methods=['POST','GET'])
-def phone_details():
-    if request.method == 'POST':
-        jsonData = request.get_json(force=True)
-        phone = jsonData['phone']
-        if phone == "" :
-            return jsonify({'type': 'validation','message': 'phone is required','status': 0})
-            sys.exit()
-        if  phone !="":
-
-            url = 'https://api.fullcontact.com/v2/person.json?phone='+str(phone)
-            header = {"X-FullContact-APIKey": APIKey}
-            r = requests.post(url, headers=header)
-            # print(r.json())
-            return jsonify(r.json())
-            sys.exit()
-        else:
-            return jsonify({'type': 'server','message': 'Something Went wrong: Internal Server Error','status': 500})
-            sys.exit()
-    return jsonify({'type': 'Request error','message': 'Method not allowed','status': 401})
-#************ Get Contact details By Email End*******
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
 
 
+#Get Classification Purposes
+@app.route("/GetClassificationPurposes",methods = ['POST', 'GET'])
+def GetClassificationPurposes():
+    if request.method == 'GET':
+        addressStates=[]
+        classificationPurposes=client.service.GetClassificationPurposes()
+        for query in classificationPurposes:
+            generate_insert_query('tga_classification_purposes', query, cursor)
+
+        return jsonify({'type': 'Get Classification Purposes', 'responseData': str(classificationPurposes), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
+
+#Get Classification Schemes
+@app.route("/GetClassificationSchemes",methods = ['POST', 'GET'])
+def GetClassificationSchemes():
+    if request.method == 'GET':
+        addressStates=[]
+        classificationSchemes=client.service.GetClassificationSchemes()
+        print(classificationSchemes)
+        sys.exit()
+        return jsonify({'type': 'Get Classification Schemes', 'responseData': str(classificationSchemes), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
+
+#Get Contact Roles
+@app.route("/GetContactRoles",methods = ['POST', 'GET'])
+def GetContactRoles():
+    if request.method == 'GET':
+        addressStates=[]
+        contactRoles=client.service.GetContactRoles()
+
+        return jsonify({'type': 'Get Contact Roles', 'responseData': str(contactRoles), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
+
+#Get Data Managers
+@app.route("/GetDataManagers",methods = ['POST', 'GET'])
+def GetDataManagers():
+    if request.method == 'GET':
+        addressStates=[]
+        dataManagers=client.service.GetDataManagers()
+
+        for query in dataManagers:
+            generate_insert_query('tga_data_managers', query, cursor)
+
+        return jsonify({'type': 'Get Data Managers', 'responseData': str(dataManagers), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
+
+#Get Details
+@app.route("/GetDetails",methods = ['POST', 'GET'])
+def GetDetails():
+    if request.method == 'GET':
+        addressStates=[]
+        details=client.service.GetDetails()
+        print(details)
+        sys.exit()
+        for query in dataManagers:
+            generate_insert_query('tga_data_managers', query, cursor)
+
+        return jsonify({'type': 'Get Details', 'responseData': str(details), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
+
+#Get Lookup
+@app.route("/GetLookup",methods = ['POST', 'GET'])
+def GetLookup():
+    if request.method == 'GET':
+        addressStates=[]
+        lookup=client.service.GetLookup()
+        print(lookup)
+        sys.exit()
+        return jsonify({'type': 'Get Data Managers', 'responseData': str(lookup), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
+
+#Get Lookup
+@app.route("/GetRegistrationManagers",methods = ['POST', 'GET'])
+def GetRegistrationManagers():
+    if request.method == 'GET':
+        addressStates=[]
+        registrationManagers=client.service.GetRegistrationManagers()
+
+        for query in registrationManagers:
+            generate_insert_query('tga_registration_managers', query, cursor)
+
+        return jsonify({'type': 'Get Registration Managers', 'responseData': str(registrationManagers), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
+
+#Get Lookup
+@app.route("/GetValidationCodes",methods = ['POST', 'GET'])
+def GetValidationCodes():
+    if request.method == 'GET':
+        addressStates=[]
+        validationCodes=client.service.GetValidationCodes()
+
+        for query in validationCodes:
+            generate_insert_query('tga_validation_codes', query, cursor)
+
+        return jsonify({'type': 'Get Validation Codes', 'responseData': str(validationCodes), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
+
+#Get Lookup
+@app.route("/Search",methods = ['POST', 'GET'])
+def Search():
+    if request.method == 'GET':
+        addressStates=[]
+        search=client.service.Search()
+        print(search)
+        sys.exit()
+        return jsonify({'type': 'search', 'responseData': str(search), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
 
 
-#************ Get Contact details By Phone *******
-@app.route("/twitter_details", methods=['POST','GET'])
-def twitter_details():
-    if request.method == 'POST':
-        jsonData = request.get_json(force=True)
-        twitter = jsonData['twitter']
-        if twitter == "" :
-            return jsonify({'type': 'validation','message': 'phone is required','status': 0})
-            sys.exit()
-        if  twitter !="":
-            url = 'https://api.fullcontact.com/v2/person.json?twitter='+str(twitter)
-            header = {"X-FullContact-APIKey": APIKey}
-            r = requests.post(url, headers=header)
-            # print(r.json())
-            return jsonify(r.json())
-            sys.exit()
-        else:
-            return jsonify({'type': 'server','message': 'Something Went wrong: Internal Server Error','status': 500})
-            sys.exit()
-    return jsonify({'type': 'Request error','message': 'Method not allowed','status': 401})
-#************ Get Contact details By Email End*******
+#Get Lookup
+@app.route("/TransferDataManager",methods = ['POST', 'GET'])
+def TransferDataManager():
+    if request.method == 'GET':
+        addressStates=[]
+        transferDataManager=client.service.TransferDataManager()
 
+        return jsonify({'type': 'search', 'responseData': str(transferDataManager), 'status': 1})
+    else:
+        return jsonify({'type': 'request','message': 'Sorry! Method Not Allowed','status': '0'})
 
-#************ lookpu by domain *******
-@app.route("/company_details", methods=['POST','GET'])
-def company_details():
-    if request.method == 'POST':
-        jsonData = request.get_json(force=True)
-        domain = jsonData['domain']
-        if domain == "" :
-            return jsonify({'type': 'validation','message': 'domain is required','status': 0})
-            sys.exit()
-        if  domain !="":
-            url = 'https://api.fullcontact.com/v2/company/lookup.json?domain='+str(domain)
-            header = {"X-FullContact-APIKey": APIKey}
-            # data={
-            #     'domain':'fullcontact.com'
-            # }
-            r = requests.get(url,headers=header)
-            # print(r.json())
-            return jsonify(r.json())
-            sys.exit()
-        else:
-            return jsonify({'type': 'server','message': 'Something Went wrong: Internal Server Error','status': 500})
-            sys.exit()
-    return jsonify({'type': 'Request error','message': 'Method not allowed','status': 401})
-#************ Get Contact details By Email End*******
-
-
-#************ lookpu by company name *******
-@app.route("/company_name", methods=['POST','GET'])
-def company_name():
-    if request.method == 'POST':
-        jsonData = request.get_json(force=True)
-        company_name = jsonData['company_name']
-        if company_name == "" :
-            return jsonify({'type': 'validation','message': 'domain is required','status': 0})
-            sys.exit()
-        if  company_name !="":
-            url = 'https://api.fullcontact.com/v2/company/search.json?companyName=='+str(company_name)
-            header = {"X-FullContact-APIKey": APIKey}
-            r = requests.get(url,headers=header)
-            # print(r.json())
-            return jsonify(r.json())
-            sys.exit()
-        else:
-            return jsonify({'type': 'server','message': 'Something Went wrong: Internal Server Error','status': 500})
-            sys.exit()
-    return jsonify({'type': 'Request error','message': 'Method not allowed','status': 401})
-#************ Get Contact details By Email End*******
 
 def generate_insert_query(table, dictionary, cursor):
-
     input_dict = zeep.helpers.serialize_object(dictionary)
     output_dict = json.loads(json.dumps(input_dict))
 
     # Get all "keys" inside "values" key of dictionary (column names)
     columns = ', '.join([key for key, value in output_dict.items()])
-    values = ', '.join(["'" + json.dumps(value)  if isinstance(value, list) else value.replace("'", "") + "'" for key, value in output_dict.items()])
+    values = ', '.join(["'" + value.replace("'", "") + "'" for key, value in output_dict.items()])
 
 
     queryBuilder = "INSERT INTO " + table + " (" + columns + ") VALUES (" + values + ")"
-
 
     retry_flag = True
     retry_count = 0
