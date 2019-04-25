a5948855aff56af93b7263c0976c0ed59285c964
unknown <vijayks2020@gmail.com>
2019-04-25 17:36:36 +0530

    only flask..

--- application.py
+++ application.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+import sys
+import time
+# import MsSqlConnect
+import json
+
+from zeep import Client
+import zeep
+from zeep.wsse.username import UsernameToken
+
+client = Client('https://ws.staging.training.gov.au/Deewr.Tga.WebServices/OrganisationServiceV7.svc?wsdl', wsse=UsernameToken('WebService.Read', 'Asdf098'))
+
+import pyodbc
+
 app = Flask(__name__)
 
 @app.route("/")
