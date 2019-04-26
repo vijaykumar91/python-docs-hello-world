1fe1d373db23142a358f9e60bd1384f328c6c551
unknown <vijayks2020@gmail.com>
2019-04-26 18:27:13 +0530

    fresh app

--- application.py
+++ application.py
@@ -4,16 +4,16 @@ import time
 # import MsSqlConnect
 import json
 
-#from zeep import Client
+from zeep import Client
 #import zeep
 #from zeep.wsse.username import UsernameToken
 
 #client = Client('https://ws.staging.training.gov.au/Deewr.Tga.WebServices/OrganisationServiceV7.svc?wsdl', wsse=UsernameToken('WebService.Read', 'Asdf098'))
 
-import pyodbc
+#import pyodbc
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!  pyodbc again"
+    return "Hello World!  pyodbc zeep"
